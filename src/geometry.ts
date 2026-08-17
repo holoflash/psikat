3d298c00c47210788ea8e909146f78899b3982b6
Redas Jefisovas <114405740+holoflash@users.noreply.github.com>
2026-08-17 17:05:59 +0200

    chore: clean up geometry rendering code

--- src/geometry.ts
+++ src/geometry.ts
@@ -1,31 +1,24 @@
 import { s_current_zoom, ZOOM_LEVELS } from "./state";
+import { get_css_var } from "./utils/dom/get_css_var";
 
-const polarToCartesian = (r: number, degrees: number) => {
-  const radians = (degrees * Math.PI) / 180.0;
-  return [r + r * Math.cos(radians), r + r * Math.sin(radians)];
-};
-const one_half = 350;
-const one = 700;
+const HALF_SIZE = 350;
+const FULL_SIZE = 700;
 
 export function geometry() {
-  // @ts-ignore
-  // oxlint-disable
-  // playing around
-  const canvas = <HTMLCanvasElement>document.getElementById("geometry");
+  const canvas = document.getElementById("geometry") as HTMLCanvasElement;
+  if (!canvas) return;
   const ctx = canvas.getContext("2d")!;
-  canvas.height = one;
-  canvas.width = one;
+  canvas.height = FULL_SIZE;
+  canvas.width = FULL_SIZE;
   canvas.style.width = "700px";
   canvas.style.height = "700px";
-  ctx.fillStyle = "rgb(0, 0, 0, 0)";
 
-  ctx.fillRect(0, 0, one, one);
-  ctx.beginPath();
   const zoom = ZOOM_LEVELS[s_current_zoom.value];
 
-  ctx.lineWidth = 1;
-  ctx.strokeStyle = "#fff";
-  const chunk = 360 / zoom;
+  ctx.strokeStyle = get_css_var("--ACCENT");
+  const chunk = (360 * Math.PI) / 180.0 / zoom;
+
+  // Clearing out the labels before re-rendering
   const prev = document.querySelectorAll("#chart-label");
   if (prev) {
     prev.forEach((el) => {
@@ -33,17 +26,22 @@ export function geometry() {
     });
   }
 
-  for (let i = 1; i <= zoom + 1; i++) {
-    const [x, y] = polarToCartesian(one_half, chunk * i);
+  for (let i = 1; i < zoom + 1; i++) {
+    const [x, y] = polar_to_cartesian(chunk * i);
+    const [z, p] = polar_to_cartesian(chunk * (i - 1));
+    ctx.moveTo(HALF_SIZE, HALF_SIZE);
+    ctx.lineTo(z, p);
     ctx.lineTo(x, y);
 
-    if (i < zoom + 1) {
-      const label = document.createElement("p");
-      label.textContent = `${i}/${zoom}`;
-      label.id = "chart-label";
-      label.style.transform = `translate(${x}px, ${y}px)`;
-      document.body.append(label);
-    }
+    const label = document.createElement("p");
+    label.textContent = `${i}`;
+    label.id = "chart-label";
+    label.style.transform = `translate(${x}px, ${y}px)`;
+    document.body.append(label);
   }
   ctx.stroke();
+}
+
+function polar_to_cartesian(rad: number) {
+  return [HALF_SIZE + HALF_SIZE * Math.cos(rad), HALF_SIZE + HALF_SIZE * Math.sin(rad)];
 }
