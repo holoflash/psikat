2e30c0cf3635bb32b6ec4590f959830841b66cbf
Redas Jefisovas <114405740+holoflash@users.noreply.github.com>
2026-08-29 08:37:17 +0200

    chore(remove js): keep only essentials

--- js_reference_code/commands/toggle_command_palette.ts
+++
@@ -1,5 +0,0 @@
-import { s_command_palette_open } from "../state";
-
-export function toggle_command_palette() {
-  s_command_palette_open.value = !s_command_palette_open.value;
-}
--- js_reference_code/commands/toggle_playing.ts
+++
@@ -1,5 +0,0 @@
-import { s_playing } from "../state";
-
-export function toggle_playing() {
-  s_playing.value = !s_playing.value;
-}
